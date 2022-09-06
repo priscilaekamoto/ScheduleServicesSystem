b7f714b71658dda13e88f041854e40d939069df2
priscilaekamoto <ekamoto.priscila@gmail.com>
2022-09-05 21:42:46 -0400

    melhorias tela de login

--- flutter-project/lib/pages/login_page.dart
+++ flutter-project/lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
@@ -12,26 +13,47 @@ class LoginPage extends StatelessWidget {
 
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
           Image.asset("assets/logo_nome.png",
           width: 250
           ),
           Text('LOGIN',
-              style: const TextStyle(fontWeight: FontWeight.bold, height: 4, fontSize: 20)
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
+          ),
+          Text('',
+              style: const TextStyle(height: 2)
           ),
           Text('Informe  seu nome e senha. Caso não possua cadastro, clique em "Cadastre-se" para criar sua conta.',
-              textAlign: TextAlign.center),
-          TextField(decoration: InputDecoration(labelText:'Login'),
+              textAlign: TextAlign.center,
+          ),
+          Text('',
+              style: const TextStyle(height: 2)
+          ),
+          TextField(decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText:'Nome'),
+          ),
+          Text('',
+              style: const TextStyle(height: 2)
           ),
           TextField(
               decoration: InputDecoration(
+                  border: OutlineInputBorder(),
                   labelText: ('Senha'),
           ),
             obscureText:true,
           ),
+          Text('',
+            style: const TextStyle(height: 2,  ),
+
+          ), // TODO: Resolver problema de alinhamento do componente pai
+          Text('Esqueci minha senha                                                          ',
+              style: const TextStyle(height: 2, color: Colors.red)
+          ),
+
           SizedBox(height: 15),
-          ElevatedButton(onPressed: () {}, child: Text('Login'))
+          ElevatedButton(onPressed: () {}, child: Text('Login'), style: ButtonStyle(),)
 
         ],
       ),
