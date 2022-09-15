b68e9a2c49f208b520d27d8eef7b47f91b0f8628
priscilaekamoto <ekamoto.priscila@gmail.com>
2022-09-14 21:55:28 -0400

    melhoria alinhamento de texto "Cadastro" tela cadastrocliente.

--- flutter-project/lib/pages/cadastro_cliente_page.dart
+++ flutter-project/lib/pages/cadastro_cliente_page.dart
@@ -7,41 +7,87 @@ class CadastroClientePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-      padding: EdgeInsets.only(top: 60, left: 30, right: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Image.asset("assets/logo_assinatura.png",
-            width:200
-
-          ),
-          Text('CADASTRO',
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
-
-          ),
-         SizedBox(height: 25),
-      Container(
-        height: 45,
-        width: 400,
-        //padding: EdgeInsets.symmetric(horizontal: 1),
-        child: TextField(decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: ('Nome')
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image.asset("assets/logo_assinatura.png", width: 200),
+            Text('CADASTRO',
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            SizedBox(height: 25),
+            Container(
+              height: 45,
+              width: 400,
+              //padding: EdgeInsets.symmetric(horizontal: 1),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: ('Nome')),
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              height: 45,
+              width: 400,
+              //padding: EdgeInsets.symmetric(horizontal: 1),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: ('Telefone')),
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              height: 45,
+              width: 400,
+              //padding: EdgeInsets.symmetric(horizontal: 1),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: ('Nascimento')),
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              height: 45,
+              width: 400,
+              //padding: EdgeInsets.symmetric(horizontal: 1),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: ('Senha')),
+              ),
+            ),
+            SizedBox(height: 25),
+            ButtonTheme(
+              height: 60.0,
+              minWidth: 250.0,
+              child: RaisedButton(
+                onPressed: () =>
+                    {Navigator.of(context).pushReplacementNamed('/login')},
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                child: Text("CADASTRAR",
+                    style: TextStyle(color: Colors.white70, fontSize: 15)),
+              ),
+            ),
+            SizedBox(height: 25),
+            ButtonTheme(
+              height: 60.0,
+              minWidth: 250.0,
+              child: RaisedButton(
+                onPressed: () =>
+                    {Navigator.of(context).pushReplacementNamed('/login')},
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                child: Text("VOLTAR",
+                    style: TextStyle(color: Colors.black, fontSize: 15)),
+                color: Colors.white70,
+              ),
+            ),
+          ],
         ),
-
-
-        ),
-      )
-
-        ],
-
-
       ),
-
-
-    ),
     );
   }
 }
--- flutter-project/lib/pages/splash_page.dart
+++ flutter-project/lib/pages/splash_page.dart
@@ -4,37 +4,27 @@ import 'package:flutter/cupertino.dart';
 class SplashPage extends StatefulWidget {
   const SplashPage({Key key}) : super(key: key);
 
- @override
+  @override
   _SplashPageState createState() => _SplashPageState();
- }
+}
 
- class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3))
+        .then((_) => Navigator.of(context).pushReplacementNamed('/login'));
+  }
 
- @override
- void initState() {
-  super.initState();
-  Future.delayed(Duration(seconds: 3)).then(
-          (_) => Navigator.of(context).pushReplacementNamed('/login')
-  );
- }
-
- @override
- Widget build(BuildContext context) {
-
-    return
-
-     Container(
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       height: 200,
-
-
-     decoration: BoxDecoration(
-      color: Colors.white,
-
-      image: DecorationImage(
-       image: AssetImage("assets/logo_nome.png"),
-       alignment: Alignment(0.0,-0.2)
-      )
-     ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          image: DecorationImage(
+              image: AssetImage("assets/logo_nome.png"),
+              alignment: Alignment(0.0, -0.2))),
     );
   }
- }
+}
