0ddbbc05bd21d658d755bc1375bb47efc6dc5a96
priscilaekamoto <ekamoto.priscila@gmail.com>
2022-09-23 19:39:34 -0400

    btão "meu agendamento" e btão "sair".

--- flutter-project/lib/pages/lista_servicos_page.dart
+++ flutter-project/lib/pages/lista_servicos_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/painting.dart';
 class ItemService {
   int index;
   String value;
+
   ItemService(int id, String val) {
     index = id;
     value = val;
@@ -12,17 +13,15 @@ class ItemService {
 }
 
 class ListaServicosPage extends StatelessWidget {
-
   const ListaServicosPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     String _selectedValue;
 
     List<ItemService> listaServicos = [
-      new ItemService(1,"Depilação axila"),
-      new ItemService(2,"Depialção buço"),
+      new ItemService(1, "Depilação axila"),
+      new ItemService(2, "Depialção buço"),
       new ItemService(3, "Depilação meia perna"),
       new ItemService(4, "Depilação nariz"),
     ];
@@ -33,7 +32,7 @@ class ListaServicosPage extends StatelessWidget {
           width: double.infinity,
           //color: Colors.red,
           padding: EdgeInsets.symmetric(vertical: 70.0, horizontal: 70.0),
-        /*  decoration: BoxDecoration(
+          /*  decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("assets/fundo_app.png"),
               fit: BoxFit.cover,
@@ -54,23 +53,46 @@ class ListaServicosPage extends StatelessWidget {
                   //isExpanded: false,
                   onChanged: (item) {
                     _selectedValue = item.toString();
-
                   },
-                  items: listaServicos
-                      .map((ItemService item) {
+                  items: listaServicos.map((ItemService item) {
                     return DropdownMenuItem(
-
                       value: item.index,
                       child: Text(
                         item.value,
                       ),
                     );
                   }).toList(),
-                )
+                ),
+                SizedBox(height: 290),
+                ButtonTheme(
+                  height: 60.0,
+                  minWidth: 250.0,
+                  child: RaisedButton(
+                    onPressed: () =>
+                        {Navigator.of(context).pushReplacementNamed('/login')},
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    child: Text("MEUS AGENDAMENTOS",
+                        style: TextStyle(color: Colors.white70, fontSize: 15)),
+                  ),
+                ),
+                SizedBox(height: 25),
+                ButtonTheme(
+                  height: 60.0,
+                  minWidth: 250.0,
+                  child: RaisedButton(
+                    onPressed: () =>
+                        {Navigator.of(context).pushReplacementNamed('/login')},
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    child: Text("SAIR",
+                        style: TextStyle(color: Colors.black, fontSize: 15)),
+                    color: Colors.white70,
+                  ),
+                ),
               ],
             ),
-          )
-      ),
+          )),
     );
   }
 }
